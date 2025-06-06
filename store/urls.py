88d05c69b1a4022c9fe18c4dc79751e593bc758d
Onyekachiii <stiloonwa4u@yahoo.com>
2025-06-06 17:42:05 +0400

    first commit

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from store import views 
+
+app_name = "store"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("detail/<slug>/", views.product_detail, name="product_detail")
+]
